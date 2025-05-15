68d6096508fff7e9cf24d347ad05c45fac6e2c56
mikecode <miguelalbertoaz@gmail.com>
2025-05-15 20:56:59 +0200

    agrega pagina blog

--- src/helpers/handleMenuMobile.ts
+++ src/helpers/handleMenuMobile.ts
@@ -1,6 +1,8 @@
-const btn = document.getElementById('menu-mobile-btn')
-const nav = document.getElementById('header-nav')
+document.addEventListener("astro:page-load", () => {
+  const btn = document.getElementById("menu-mobile-btn");
+  const nav = document.getElementById("header-nav");
 
-btn?.addEventListener('click', () => {
-    nav?.classList.toggle('active')
-})
+  btn?.addEventListener("click", () => {
+    nav?.classList.toggle("active");
+  });
+});
